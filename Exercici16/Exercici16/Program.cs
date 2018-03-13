bb7e81fa948f80c472aeacea744ce31d2031bc28
Sergi <sergipuigmitja@gmail.com>
2018-03-13 21:14:28 +0100

    Versio 1

---
+++ Exercici16/Exercici16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercici16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String opcio;
+            MostrarOpcions();
+            opcio = Console.ReadLine();
+
+        }
+        private static void MostrarOpcions()
+        {
+            Console.WriteLine("Opcio {0}: {1}", 1, "Buscaminas");
+            Console.WriteLine("Opcio {0}: {1}", 2, "Solitario");
+            Console.WriteLine("Opcio {0}: {1}", 3, "Blackjack");
+            Console.WriteLine("Opcio {0}: {1}", 4, "Poker");
+        }
+    }
+}
